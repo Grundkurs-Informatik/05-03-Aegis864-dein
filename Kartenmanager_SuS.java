cfae36d6da8821330973da016785704718e42f06
Aegis864-dein <baderemil@icloud.com>
2025-03-05 09:03:33 +0100

    Update Kartenmanager_SuS.java

--- Kartenmanager_SuS.java
+++ Kartenmanager_SuS.java
@@ -12,6 +12,7 @@ import sum.strukturen.*;
  * - Sortieren/Erneuern (Platzhalter) 
  * - Zeichnen ab y=150
  */
+//testtest
 public class Kartenmanager_SuS extends Grafik {
 
     // -----------------------------
